b8b6a6d28b0f9c29a44b51873bb3b72aa5955574
Thierry N <thierryn1 at hispeed dot ch>
2009-08-08 18:54:59 +0200

    Ajout de jdk#6u14-1

---
+++ base/jdk/jdk.sh
@@ -0,0 +1,21 @@
+# Begin /etc/profile.d/jdk.sh
+
+# Set JAVA_HOME directory
+JAVA_HOME=/opt/jdk
+
+# Adjust PATH
+pathappend ${JAVA_HOME}/bin PATH
+
+# Auto Java CLASSPATH
+# Copy jar files to, or create symlinks in this directory
+AUTO_CLASSPATH_DIR=/usr/lib/classpath
+pathprepend . CLASSPATH
+for dir in `find ${AUTO_CLASSPATH_DIR} -type d 2>/dev/null`; do
+    pathappend $dir CLASSPATH
+done
+
+export JAVA_HOME CLASSPATH
+unset AUTO_CLASSPATH_DIR
+unset dir
+
+# End /etc/profile.d/jdk.sh
