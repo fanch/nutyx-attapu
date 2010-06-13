1a4edff9e0f79c86f76a1c1d0604cbbe9934e4f2
fanch <fanchyannmaria at orange dot fr>
2010-06-13 23:05:49 +0200

    mise à jour k3b#1.92.0rc3-1

--- kde/k3b/qt3.sh
+++ kde/k3b/qt3.sh
@@ -1,5 +1,5 @@
 export QTDIR=/usr/
 if [ -d $QTDIR ]; then
-	export PATH=$QTDIR/bin:$PATH
-	export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:$QTDIR/lib/pkgconfig
+export PATH=$QTDIR/bin:$PATH
+export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:$QTDIR/lib/pkgconfig
 fi
